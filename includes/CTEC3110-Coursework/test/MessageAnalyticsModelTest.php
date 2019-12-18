5f99f2fe26705c2dd06c0778924592d2365a0ced
kmcanos <35694512+kmcanos@users.noreply.github.com>
2019-12-18 15:05:19 +0000

    Completed current DatabaseWrapper Functions

--- includes/CTEC3110-Coursework/test/MessageAnalyticsModelTest.php
+++ includes/CTEC3110-Coursework/test/MessageAnalyticsModelTest.php
@@ -12,46 +12,68 @@
 namespace M2MConnect;
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
+use \VerticalBarChart;
 
 
 class MessageAnalyticsModelTest extends TestCase
 {
 
     protected $config;
+    protected $test_db_config;
+    protected $libchartPath;
 
     protected function setUp(): void
     {
         $this->config = require("../app/settings.php");
+        $this->test_db_config = (TEST_DB_SETTINGS['pdo_test_settings']);
+        $this->libchartPath = (LIB_CHART_CLASS_PATH);
+
 
     }
     public function test__construct()
     {
-        $TestAnalytics = new MessageAnalyticsModel();
+        $testAnalytics = new MessageAnalyticsModel();
 
         $this->assertInstanceOf(MessageAnalyticsModel::class,$TestAnalytics);
-        $this->assertEmpty($TestAnalytics->getVars()[0]);
-        $this->assertEmpty($TestAnalytics->getVars()[1]);
-        $this->assertEmpty($TestAnalytics->getVars()[2]);
+        $this->assertEmpty($testAnalytics->getVars()[0]);
+        $this->assertEmpty($testAnalytics->getVars()[1]);
+        $this->assertEmpty($testAnalytics->getVars()[2]);
     }
 
     public function testSetStoredMessageData()
     {
-        $TestAnalytics = new MessageAnalyticsModel();
+        $testAnalytics = new MessageAnalyticsModel();
+        $testMessageWrapper = new DatabaseWrapper();
+        $testMessageWrapper->setDatabaseConnectionSettings($this->test_db_config);
+        $testMessages = $testMessageWrapper->getMessages();
 
-        $this->assertSame([23,21],$TestAnalytics->setStoredMessageData([23,21]));
+        $this->assertNull($testAnalytics->setStoredMessageData($testMessages));
 
 
     }
+
     public function testCreateBarChart()
     {
+        $testAnalytics = new MessageAnalyticsModel();
 
+
+        $testMessageWrapper = new DatabaseWrapper();
+        $testMessageWrapper->setDatabaseConnectionSettings($this->test_db_config);
+        $testMessages = $testMessageWrapper->getMessages();
+        $testAnalytics->setStoredMessageData($testMessages);
+
+        $testAnalytics->createBarChart();
+
+        $this->assertDirectoryExists("/p3t/phpappfolder/public_php/CTEC3110-Coursework/media/charts/");
+        $this->assertFileExists("../p3t/phpappfolder/public_php/CTEC3110-Coursework/media/charts/message-barchart.png");
     }
 
     public function testCreatePieChart()
     {
 
     }
+
+
 
     public function testCreateLineChart()
     {
